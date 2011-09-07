5f4462cf459f3921e1add5500deaa59f4fa93f2a
Dan Christian <robodan65@gmail.com>
2011-09-07 17:32:44 -0600

    Organize as a class

--- tagboy.py
+++ tagboy.py
@@ -24,96 +24,103 @@ import sys
 # hmm? use backported argparse: http://code.google.com/p/argparse/
 import optparse                 # because argparse requires python >= 2.7
 
-def ReadMetadata(fname):
-    """Read file metadata and return."""
-    metadata = ex.ImageMetadata(fname)
-    try:
-        metadata.read()
-    except IOError:
-        print "Error reading: %s" % fname
-        metadata = None         # force object distruction
-    return metadata
+class TagBoy(object):
+    FILENAME = '_filename_'
+    FILEPATH = '_filepath_'
 
-def FlattenTags(metadata):
-    """Convert all tags to a dictionary with a uniform naming style.
-
-    We store both the fully qualified name and the short name.
-    For short names, XMP has precedence over IPTC over EXIF.
-    """
-    uni = dict()
-    # BUG? XMP has a copy of everything in EXIF
-    for k in metadata.exif_keys:
-        kwords = k.split('.')
+    def ReadMetadata(self, fname):
+        """Read file metadata and return."""
+        metadata = ex.ImageMetadata(fname)
         try:
-            v = metadata[k].raw_value
-        except ex.ExifValueError:
-            continue
-        uni[k] = v
-        uni[kwords[-1]] = v
-    for k in metadata.iptc_keys:
-        kwords = k.split('.')
-        try:
-            v = metadata[k].raw_value
-        except ex.IptcValueError:
-            continue
-        uni[k] = v
-        uni[kwords[-1]] = v
-    for k in metadata.xmp_keys:
-        kwords = k.split('.')
-        try:
-            v = metadata[k].raw_value
-        except ex.XmpValueError:
-            continue
-        uni[k] = v
-        uni[kwords[-1]] = v
-    return uni
+            metadata.read()
+        except IOError:
+            print "Error reading: %s" % fname
+            metadata = None         # force object distruction
+        return metadata
 
-def HandleArgs(args):
-    """Setup argument parsing and return parsed arguments."""
-    parser = optparse.OptionParser()
-    parser.add_option("--name", help="Match files with this name (repeatable)",
-                      action="append", dest="nameGlobs", default=[])
-    parser.add_option("--echo", help="Echo argument with $vars (repeatable)",
-                      action="append", dest="echoStrings", default=[])
-    parser.add_option("--ls", help="Show image info",
-                      action="store_true", dest="ls", default=False)
-    parser.add_option("-v", "--verbose", help="Show more detail",
-                      action="store_true", dest="verbose", default=False)
-    return parser.parse_args(args)
+    def FlattenTags(self, metadata):
+        """Convert all tags to a dictionary with a uniform naming style.
 
-def PrintKeyValue(d, verbose):
-    """Pretty print key-values."""
-    for k in sorted(d.keys()):
-        if not verbose and k.find('.') >= 0:
-            continue            # skip the dotted names
-        print "%40s: %s" % (k, d[k])
+        We store both the fully qualified name and the short name.
+        For short names, XMP has precedence over IPTC over EXIF.
+        """
+        uni = dict()
+        # BUG? XMP has a copy of everything in EXIF
+        for k in metadata.exif_keys:
+            kwords = k.split('.')
+            try:
+                v = metadata[k].raw_value
+            except ex.ExifValueError:
+                continue
+            uni[k] = v
+            uni[kwords[-1]] = v
+        for k in metadata.iptc_keys:
+            kwords = k.split('.')
+            try:
+                v = metadata[k].raw_value
+            except ex.IptcValueError:
+                continue
+            uni[k] = v
+            uni[kwords[-1]] = v
+        for k in metadata.xmp_keys:
+            kwords = k.split('.')
+            try:
+                v = metadata[k].raw_value
+            except ex.XmpValueError:
+                continue
+            uni[k] = v
+            uni[kwords[-1]] = v
+        return uni
 
-FILENAME = '_filename_'
-FILEPATH = '_filepath_'
+    def HandleArgs(self, args):
+        """Setup argument parsing and return parsed arguments."""
+        parser = optparse.OptionParser()
+        parser.add_option("--name", help="Match files with this name (repeatable)",
+                          action="append", dest="nameGlobs", default=[])
+        parser.add_option("--echo", help="Echo argument with $vars (repeatable)",
+                          action="append", dest="echoStrings", default=[])
+        parser.add_option("--ls", help="Show image info (more with -v)",
+                          action="store_true", dest="ls", default=False)
+        parser.add_option("-v", "--verbose", help="Show more detail",
+                          action="store_true", dest="verbose", default=False)
+        (self.options, pos_args) = parser.parse_args(args)
+        return pos_args
 
-def EachFile(fn, options):
-    meta = ReadMetadata(fn)
-    if not meta:
-        return
-    unified = FlattenTags(meta)
-    unified[FILEPATH] = fn
-    unified[FILENAME] = os.path.basename(fn)
-    if options.ls:
-        print "========= %s =========" % (fn)
-        PrintKeyValue(unified, options.verbose)  # DEBUG
-    for ss in options.echoStrings:
-        out = string.Template(ss).safe_substitute(unified)
-        print out
+    def PrintKeyValue(self, d):
+        """Pretty print key-values."""
+        for k in sorted(d.keys()):
+            if not self.options.verbose and k.find('.') >= 0:
+                continue            # skip the dotted names
+            print "%40s: %s" % (k, d[k])
+
+
+    def EachFile(self, fn):
+        meta = self.ReadMetadata(fn)
+        if not meta:
+            return
+        unified = self.FlattenTags(meta)
+        unified[self.FILEPATH] = fn
+        unified[self.FILENAME] = os.path.basename(fn)
+        if self.options.ls:
+            print "========= %s =========" % (fn)
+            self.PrintKeyValue(unified)
+        for ss in self.options.echoStrings:
+            # TODO: don't compile each time
+            # TODO: subclass Template to all . in variable pattern
+            out = string.Template(ss).safe_substitute(unified)
+            print out
     
 def main():
-    (options, args) = HandleArgs(sys.argv[1:])
-    print "tagboy begins:", options, args # DEBUG
-    if options.nameGlobs:
-        print "Oops:  --name isn't supported yet"
+    tb = TagBoy()
+    args = tb.HandleArgs(sys.argv[1:])
+    if not args:
+        print "No arguments.  Nothing to do."
+        return
+    print "tagboy begins:", args # DEBUG
     for fn in args:
         # TODO: if ends in / (or \\), do directory traverse
         # TODO: check name against nameGlob, inameGlob, nameRegex
-        EachFile(fn, options)
+        tb.EachFile(fn)
 
 if __name__ == '__main__':
     main()
