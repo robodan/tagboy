87c6ae9e68bf9ebe55b5896f651b3c0218cec42e
Dan Christian <robodan65@gmail.com>
2011-09-06 20:49:34 -0600

    Add --echo option

--- tagboy.py
+++ tagboy.py
@@ -5,18 +5,21 @@
 # 5 Sept 2011
 
 # Features:
-# Be able to take filename on the command line or stdin (e.g. find)
+# Be able to take filename on the command line (??? or stdin (e.g. find))
 # Be able to search directory trees for files matching patterns
 # Be able to select files based on tag patterns
 # Be able to write/insert/append to tag fields (include from other fields)
 
 """As cowboys wrangle cows, tagboy wrangles EXIF/IPTC/XMP tags in images.
+
 Usage:
   tagboy --match '*.location=Phoenix' --print
 """
 
-import sys
+import os
 import pyexiv2 as ex
+import string
+import sys
 
 # hmm? use backported argparse: http://code.google.com/p/argparse/
 import optparse                 # because argparse requires python >= 2.7
@@ -69,7 +72,9 @@ def HandleArgs(args):
     """Setup argument parsing and return parsed arguments."""
     parser = optparse.OptionParser()
     parser.add_option("--name", help="Match files with this name (repeatable)",
-                      action="append", dest="nameGlob", default=[])
+                      action="append", dest="nameGlobs", default=[])
+    parser.add_option("--echo", help="Echo argument with $vars (repeatable)",
+                      action="append", dest="echoStrings", default=[])
     parser.add_option("--ls", help="Show image info",
                       action="store_true", dest="ls", default=False)
     parser.add_option("-v", "--verbose", help="Show more detail",
@@ -83,19 +88,27 @@ def PrintKeyValue(d, verbose):
             continue            # skip the dotted names
         print "%40s: %s" % (k, d[k])
 
+FILENAME = '_filename_'
+FILEPATH = '_filepath_'
+
 def EachFile(fn, options):
     meta = ReadMetadata(fn)
     if not meta:
         return
     unified = FlattenTags(meta)
+    unified[FILEPATH] = fn
+    unified[FILENAME] = os.path.basename(fn)
     if options.ls:
         print "========= %s =========" % (fn)
         PrintKeyValue(unified, options.verbose)  # DEBUG
+    for ss in options.echoStrings:
+        out = string.Template(ss).safe_substitute(unified)
+        print out
     
 def main():
     (options, args) = HandleArgs(sys.argv[1:])
     print "tagboy begins:", options, args # DEBUG
-    if options.nameGlob:
+    if options.nameGlobs:
         print "Oops:  --name isn't supported yet"
     for fn in args:
         # TODO: if ends in / (or \\), do directory traverse
